ddd468e411e378adcf6aad9f32c7600a608e8a71
keerthu1991 <keerthanambika03@gmail.com>
2021-01-11 15:59:16 +0530

    image changes

--- src/App.js
+++ src/App.js
@@ -15,38 +15,37 @@ import Locations from './components/pages/Locations';
 import News from './components/pages/News';
 import PatientPtl from './components/pages/PatientPtl';
 import RequestApt from './components/pages/RequestApt';
+import Footer from './components/layout/Footer';
+import BgImage from './components/layout/Bgimage';
 import './App.css';
 
 
 function App() {
   return (
-    <Router>
-      
-     <AidcpcMenu/>
-     
-     <Switch>
-       <Route path='/home' exact component={Home}/>
-       <Route path='/aidcpc' exact component={Aidcpc}/>
-       <Route path='/healthprofessionals' exact component={HealthProfessionals}/>
-       <Route path='/infdisease' exact component={InfDisease}/>
-       <Route path='/apn' exact component={ApNurse}/>
-       <Route path='/billing' exact component={Billing}/>
-       <Route path='/career' exact component={Career}/>
-       <Route path='/credentials' exact component={Credentials}/>
-       <Route path='/faqs' exact component={Faqs}/>
-       <Route path='/forms' exact component={Forms}/>
-       <Route path='/locations' exact component={Locations}/>
-       <Route path='/news' exact component={News}/>
-       <Route path='/patientptl' exact component={PatientPtl}/>
-       <Route path='/requestapt' exact component={RequestApt}/>
-     </Switch>
-      <footer className="footer-bg">
-        <span className="contact">Contact:(732) 514-9624</span>|
-        <span className="location">Location: NJ</span>|
-        <span className="copyright">Copyright &copy; 2020</span>
-      </footer>
-   </Router>
     
+      <Router>
+       
+      
+      <AidcpcMenu/>
+      <BgImage/>
+      <Switch>
+        <Route path='/home' exact component={Home}/>
+        <Route path='/aidcpc' exact component={Aidcpc}/>
+        <Route path='/healthprofessionals' exact component={HealthProfessionals}/>
+        <Route path='/infdisease' exact component={InfDisease}/>
+        <Route path='/apn' exact component={ApNurse}/>
+        <Route path='/billing' exact component={Billing}/>
+        <Route path='/career' exact component={Career}/>
+        <Route path='/credentials' exact component={Credentials}/>
+        <Route path='/faqs' exact component={Faqs}/>
+        <Route path='/forms' exact component={Forms}/>
+        <Route path='/locations' exact component={Locations}/>
+        <Route path='/news' exact component={News}/>
+        <Route path='/patientptl' exact component={PatientPtl}/>
+        <Route path='/requestapt' exact component={RequestApt}/>
+      </Switch>
+      <Footer/>
+    </Router>
   );
 }
 
---
+++ src/components/layout/Bgimage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Bgimage.css';
+
+function Bgimage() {
+    return (
+        <>
+            <body>
+                Hello All!
+            </body>
+        </>
+    )
+}
+
+export default Bgimage
