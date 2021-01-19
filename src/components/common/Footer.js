11bb9aac0d38fc275207a918f9bde681c05b3572
keerthu1991 <keerthanambika03@gmail.com>
2021-01-19 18:58:37 +0530

    developer

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -6,7 +6,12 @@ function Footer() {
       <footer className="footer-bg">
         <span className="contact">Contact:(732) 514-9624</span>|
         <span className="location">Location: NJ</span>|
-        <span className="copyright">Copyright &copy; 2020</span>
+        <span className="copyright">Copyright &copy; 2020</span>|
+        <span className="developer">
+          <a href="https://www.linkedin.com/in/keerthanambika-n-60164454/">
+            Developed by Keerthanambika N
+          </a>
+        </span>
       </footer>
     </>
   );
