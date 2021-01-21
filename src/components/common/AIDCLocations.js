b8101ddfee4f8f7494a89a769dc91e169cc15dd2
keerthu1991 <keerthanambika03@gmail.com>
2021-01-21 22:24:31 +0530

    title update

--- src/components/common/AIDCLocations.js
+++ src/components/common/AIDCLocations.js
@@ -7,6 +7,7 @@ export default function AIDCLocations() {
     <Container className="aidclocations">
       <Card>
         <iframe
+          title="Locations for Office,Hospitals,Nursing Homes"
           src="https://www.google.com/maps/d/u/0/embed?mid=1UV9EHqqbOzR7O2S2pRqZvchpByjWEDOf"
           width="1200"
           height="400"
