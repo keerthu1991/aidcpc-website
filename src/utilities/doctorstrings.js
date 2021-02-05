b1f30acea15c1766b16417ca473a3765099aae0e
keerthu1991 <keerthanambika03@gmail.com>
2021-02-05 19:37:37 +0530

    changes

--- src/utilities/doctorstrings.js
+++ src/utilities/doctorstrings.js
@@ -3,7 +3,7 @@ const drCharu = [
     title: 'Dr Rathnakumar Charumathi, MD',
     role: 'President and Founder , Advanced Infectious Diseases',
     description:
-      'Dr. Charumathi Rathnakumar is an Infectious Disease Specialist in Edison, New Jersey. She graduated with honors in 1993. Having more than 23 years of diverse experiences, especially in Infectious Disease, Internal Medicine, Dr. Charumathi Rathnakumar affiliates with many hospitals including Community Hospital Group Inc, Robert Wood Johnson University Hospital Rahway, and cooperates with other doctors and specialists in medical group Advanced Infectious Diseases Consultants, Pc. Call Dr. Charumathi Rathnakumar on phone number (732) 514-9624 for more information and advises or to book an appointment.',
+      'Dr. Charumathi Rathnakumar is an Infectious Disease Specialist in Edison, New Jersey. She is board certified in Internal Medicine and Infectious Diseases. She graduated with honors in 1993. Having more than 28 years of diverse experience, especially in Infectious Disease, Internal Medicine, Dr. Charumathi Rathnakumar affiliates with many hospitals including Hackensack Meridian Health - JFK Medical Center, Edison, NJ, Robert wood Johnson University Hospital, Rahway, NJ and Kindred Hospital Robert Wood Johnson University Hospital Rahway. Please call (732) 230 2828 to book an appointment.',
     image: 'images/charu.jpg',
     href:
       'https://www.healthgrades.com/physician/dr-charumathi-rathnakumar-3kn8v',
