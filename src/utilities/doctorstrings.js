c578d12bbd5300048c9682299f3bd8ae7b443ad7
keerthu1991 <keerthanambika03@gmail.com>
2021-02-05 19:41:45 +0530

    remove href

--- src/utilities/doctorstrings.js
+++ src/utilities/doctorstrings.js
@@ -5,8 +5,6 @@ const drCharu = [
     description:
       'Dr. Charumathi Rathnakumar is an Infectious Disease Specialist in Edison, New Jersey. She is board certified in Internal Medicine and Infectious Diseases. She graduated with honors in 1993. Having more than 28 years of diverse experience, especially in Infectious Disease, Internal Medicine, Dr. Charumathi Rathnakumar affiliates with many hospitals including Hackensack Meridian Health - JFK Medical Center, Edison, NJ, Robert wood Johnson University Hospital, Rahway, NJ and Kindred Hospital, Robert Wood Johnson University Hospital, Rahway. Please call (732) 230 2828 to book an appointment.',
     image: 'images/charu.jpg',
-    href:
-      'https://www.healthgrades.com/physician/dr-charumathi-rathnakumar-3kn8v',
     align: 'right',
   },
 ];
@@ -52,8 +50,6 @@ const drYasar = [
     title: 'Dr. Mukaddes Yasar, MD',
     description:
       'Dr. Mukaddes Yasar specializes in infectious disease and has over 14 years of experience in the field of medicine and Infectious Diseases.  She is affiliated with numerous hospitals, including Robert Wood Johnson University Hospital, Rahway (NJ) and JFK Medical Center, Edison (NJ).',
-    href:
-      'https://doctors.hackensackmeridianhealth.org/provider/Mukaddes+Yasar/1319777',
     align: 'left',
     image: 'images/yasar.png',
   },
